64621552e0cd921484deeb8d0634b30b6c9950b1
Ivan Zabrodin <pyrat32@gmail.com>
2021-01-21 21:10:33 -0600

    main bit works

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let times = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+let times24 = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+setInterval(updateTexts, 500)
 
 if (!getLocal()) {
     let schedule = []
@@ -32,11 +34,19 @@ function updateTexts() {
                 elem => elem.time == $(this).attr('id')
             ).message
         )
+        let time24 = times24[times.indexOf($(this).attr('id'))]
+        let real24 = parseInt(moment().format('H'))
+        if (time24 < real24) {
+            $(this).attr('class', 'past')
+        } else if (time24 > real24) {
+            $(this).attr('class', 'future')
+        } else {
+            $(this).attr('class', 'present')
+        }
     })
-    $('#currentDay').text(
-        
-    )
+    $('#currentDay').text(moment().format('LLLL'))
 }
+
 
 
 function getLocal() { return JSON.parse(localStorage.getItem('schedule')) }
